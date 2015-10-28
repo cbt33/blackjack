d91a1b68f17cdd78bf332776b83414ffbb5f5d75
cbt33 <cbt33@users.noreply.github.com>
2015-10-28 10:10:26 -0400

    Update Strategy.java

--- src/blackjack/Strategy.java
+++ src/blackjack/Strategy.java
@@ -5,24 +5,30 @@ import static blackjack.Choice.*;
 public class Strategy {
 	
 	private static final Choice[][]  hardHandMap = {{HIT, HIT, HIT, HIT, HIT, HIT, HIT, HIT, HIT, HIT,}, //5-8
-												{HIT, DDH, DDH, DDH, DDH, HIT, HIT, HIT, HIT, HIT},  //9
-												{DDH, DDH, DDH, DDH, DDH, DDH, DDH, DDH, HIT, HIT},  //10
-												{DDH, DDH, DDH, DDH, DDH, DDH, DDH, DDH, DDH, HIT},  //11
-												{HIT, HIT, STA, STA, STA, HIT, HIT, HIT, HIT, HIT},  //12
-												{STA, STA, STA, STA, STA, HIT, HIT, HIT, HIT, HIT},  //13
-												{STA, STA, STA, STA, STA, HIT, HIT, HIT, HIT, HIT},  //14
-												{STA, STA, STA, STA, STA, HIT, HIT, HIT, SUR, HIT},  //15
-												{STA, STA, STA, STA, STA, HIT, HIT, SUR, SUR, SUR},};//16
+							{HIT, DDH, DDH, DDH, DDH, HIT, HIT, HIT, HIT, HIT},  //9
+							{DDH, DDH, DDH, DDH, DDH, DDH, DDH, DDH, HIT, HIT},  //10
+							{DDH, DDH, DDH, DDH, DDH, DDH, DDH, DDH, DDH, HIT},  //11
+							{HIT, HIT, STA, STA, STA, HIT, HIT, HIT, HIT, HIT},  //12
+							{STA, STA, STA, STA, STA, HIT, HIT, HIT, HIT, HIT},  //13
+							{STA, STA, STA, STA, STA, HIT, HIT, HIT, HIT, HIT},  //14
+							{STA, STA, STA, STA, STA, HIT, HIT, HIT, SUR, HIT},  //15
+							{STA, STA, STA, STA, STA, HIT, HIT, SUR, SUR, SUR},};//16
 	
-	private static final Choice[][] softHandMap = {{HIT, HIT, HIT, DDH, DDH, HIT, HIT, HIT, HIT, HIT,}, //A2
-													{HIT, HIT, HIT, DDH, DDH, HIT, HIT, HIT, HIT, HIT},  //A3
-													{HIT, HIT, DDH, DDH, DDH, DDS, DDS, DDS, HIT, HIT},  //A4
-													{HIT, DDS, DDS, DDS, DDS, DDS, DDS, DDS, DDS, HIT},  //A5
-													{HIT, HIT, STA, STA, STA, HIT, HIT, HIT, HIT, HIT},  //A6
-													{STA, STA, STA, STA, STA, HIT, HIT, HIT, HIT, HIT},};  //A7
+	private static final Choice[][] softHandMap = {{HIT, HIT, HIT, DDH, DDH, HIT, HIT, HIT, HIT, HIT}, //A2
+							{HIT, HIT, HIT, DDH, DDH, HIT, HIT, HIT, HIT, HIT},  //A3
+							{HIT, HIT, DDH, DDH, DDH, DDS, DDS, DDS, HIT, HIT},  //A4
+							{HIT, DDS, DDS, DDS, DDS, DDS, DDS, DDS, DDS, HIT},  //A5
+							{HIT, HIT, STA, STA, STA, HIT, HIT, HIT, HIT, HIT},  //A6
+							{STA, STA, STA, STA, STA, HIT, HIT, HIT, HIT, HIT},};  //A7
 	
 	
-	private static final Choice[][] splitMap = new Choice[7][10];
+	private static final Choice[][] splitMap = {{SPL, SPL, SPL, SPL, SPL, SPL, HIT, HIT, HIT, HIT}, //2-2
+						{SPL, SPL, SPL, SPL, SPL, SPL, HIT, HIT, HIT, HIT},  //3-3
+						{HIT, HIT, HIT, SPL, SPL, HIT, HIT, HIT, HIT, HIT},  //4-4
+						{SPL, SPL, SPL, SPL, SPL, HIT, HIT, HIT, HIT, HIT},  //5-5
+						{SPL, SPL, SPL, SPL, SPL, SPL, HIT, HIT, HIT, HIT},  //6-6
+						{STA, STA, STA, STA, STA, HIT, HIT, HIT, HIT, HIT},  //7-7
+						{SPL, SPL, SPL, SPL, SPL, SPL, SPL, SPL, SPL, SPL}}; //
 	
 	static {
 		//Hard hand
